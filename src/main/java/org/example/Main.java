082add2cfe91e43b0397ba22c2f49f0ffa3705d4
EricLarwa <larwae22@students.ecu.edu>
2025-11-14 17:25:54 -0500

    Initialize Default Constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
 package org.example;
+import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public String hash;
+  public String previousHash;
+  private String data;
+  private long timeStamp;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+  public Main(String data, String previousHash) {
+      this.data = data;
+      this.previousHash = previousHash;
+      this.timeStamp = new Date().getTime();
+  }
 }
